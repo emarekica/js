42c195d32a3caf7cefb749b9d488f6054620c553
emarekica <emarekica@gmail.com>
2021-09-15 21:21:10 +0200

    algebra prime numbers

---
+++ algebra/01-arrays-and-conditions/main.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const primeNumberCount = [];
+
+function countPrimeNumbers(from, to) {
+
+  for (let i = 2; i ; i++) {
+
+  }
+
+  // primeNumberCount.push();
+}
+
+
+
+
+
+// filling an empty array with FOR-loop
+
+const types = [];
+
+for(let i = 0; i <toniArray.length; i++) {
+  console.log(mariArray[i]);
+
+  // 1: Filling types array, just reading from mariArrat
+  // types[i] = typeof toniArray[i];
+
+  // 2: push method
+    types.push(typeof toniArray[i]);
+}
+
+console.log(types);
+
+
+
+
+
+
+
+
+
+
+/* function primeNumber(n) {
+  
+  for (var i = 2; i < n; i++) {
+    if(n % i === 0) {
+        return false;
+    } else if (n > 1) {
+        return true;
+    }
+   }
+}   
+
+console.log(primeNumber(3));
+console.log(primeNumber(4));
+console.log(primeNumber(5));
+console.log(primeNumber(70));
+console.log(primeNumber(32));
+
+ */
+
+
+/* function isPrime(n) {
+  if (n <= 1) return false;
+  if (n % 2 == 0 && n > 2) return false;
+    else {
+      return true;
+    }
+}
+
+console.log(isPrime(3));
+console.log(isPrime(7));
+console.log(isPrime(10));
+ */
+
