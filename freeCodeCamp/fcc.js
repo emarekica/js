c730d90362285c93b89218b73558d07206958924
emarekica <emarekica@gmail.com>
2022-01-15 18:05:18 +0100

    feat: add Record Collection challenge

--- freeCodeCamp/fcc.js
+++ freeCodeCamp/fcc.js
@@ -31,4 +31,91 @@ function golfScore(par, strokes) {
 
 golfScore(5, 4);
 
-// ------------------------
+// ------------------------ Counting Cards
+
+let count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+}
+console.log(cc(2));
+console.log(cc(3));
+console.log(cc(7));
+console.log(cc("K"));
+console.log(cc("A"));
+
+// ------------------------ Record Collection
+
+const recordCollection = {
+  2548: {
+    // id
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi", // prop: value
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
+
+function updateRecords(records, id, prop, value) {
+  // if prop isn't tracks and value isn't an empty string, update or set that album's prop to value
+  if (prop != "tracks" && value != "") {
+    records[id][prop] = value;
+  }
+
+  // if prop is tracks but the album doesn't have a tracks property (hasOwnProperty), create an empty array and add value to it
+  else if (
+    prop === "tracks" &&
+    records - id.hasOwnProperty("tracks") === false
+  ) {
+    records.id.prop = [value];
+  }
+
+  // if prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array
+  else if (prop === "tracks" && value !== "") {
+    records.id.prop.push(value);
+  }
+
+  // if value is an empty string, delete the given prop property from the album
+  else if (value === "") {
+    delete records.id.prop;
+  }
+
+  // your function must always return the entire record collection object
+  return records;
+}
+
+console.log(updateRecords(recordCollection, 5439, "artist", "ABBA"));
+
+// ------------------------ Record Collection
