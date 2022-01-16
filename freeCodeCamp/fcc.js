c0f7e27caedccdebfa7da7dfad1fe67a47ba8b51
emarekica <emarekica@gmail.com>
2022-01-16 21:08:08 +0100

    feat: recursion

--- freeCodeCamp/fcc.js
+++ freeCodeCamp/fcc.js
@@ -120,53 +120,113 @@
 
 // ------------------------ Profile Lookup
 
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
+// const contacts = [
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: "0543236543",
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: "0994372684",
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//   },
+//   {
+//     firstName: "Sherlock",
+//     lastName: "Holmes",
+//     number: "0487345643",
+//     likes: ["Intriguing Cases", "Violin"],
+//   },
+//   {
+//     firstName: "Kristian",
+//     lastName: "Vos",
+//     number: "unknown",
+//     likes: ["JavaScript", "Gaming", "Foxes"],
+//   },
+// ];
 
-function lookUpProfile(name, prop) {
-  // loop through the object
-  // if name does not correspond to any contacts then return the string No such contact
-  for (let i = 0; i < contacts.length; i++) {
-    // check if name is an actual contact's firstName
-    if (contacts[i].firstName === name) {
-      // check if the given property (prop) is a property of that contact
-      if (contacts[i].hasOwnProperty(prop)) {
-        // If both are true, then return the "value" of that property
-        return contacts[i][prop];
+// function lookUpProfile(name, prop) {
+//   // loop through the object
+//   // if name does not correspond to any contacts then return the string No such contact
+//   for (let i = 0; i < contacts.length; i++) {
+//     // check if name is an actual contact's firstName
+//     if (contacts[i].firstName === name) {
+//       // check if the given property (prop) is a property of that contact
+//       if (contacts[i].hasOwnProperty(prop)) {
+//         // If both are true, then return the "value" of that property
+//         return contacts[i][prop];
 
-        // If prop does not correspond to any valid properties of a contact found to match name then return the string No such property
-      } else {
-        return "No such property";
-      }
-    }
-  }
-  return "No such contact";
-}
+//         // If prop does not correspond to any valid properties of a contact found to match name then return the string No such property
+//       } else {
+//         return "No such property";
+//       }
+//     }
+//   }
+//   return "No such contact";
+// }
 
-console.log(lookUpProfile("Akira", "likes"));
+// console.log(lookUpProfile("Akira", "likes"));
 
 // ------------------------ Use Recursion to Create a Countdown
+
+// function countdown(n) {
+//   // base case
+//   if (n < 0) {
+//     return [];
+
+//     // recursive call
+//   } else {
+//     const count = countdown(n - 1);
+//     count.unshift(n);
+//     return count;
+//   }
+// }
+
+// console.log(countdown(1)); // [1, 0]
+// console.log(countdown(5)); // [ 5, 4, 3, 2, 1, 0 ]
+// console.log(countdown(10)); // [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+// console.log(countdown(-2)); // []
+
+// ------------------------ Use Recursion to Create a Range of Numbers
+
+function rangeOfNumbers(startNum, endNum) {
+  return startNum === endNum
+    ? [endNum]
+    : rangeOfNumbers(startNum, endNum - 1).concat(endNum);
+}
+
+// solution 1
+
+function rangeOfNumbers(startNum, endNum) {
+  return startNum === endNum
+    ? [startNum]
+    : rangeOfNumbers(startNum, endNum - 1).concat(endNum);
+}
+
+// solution 2
+
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum - startNum === 0) {
+    return [endNum];
+  } else {
+    var numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
+
+// solution 3
+
+function rangeOfNumbers(startNum, endNum) {
+  if (startNum == endNum) {
+    return [endNum];
+  } else {
+    const arr = rangeOfNumbers(startNum + 1, endNum);
+    arr.unshift(startNum);
+    return arr;
+  }
+}
+
+console.log(rangeOfNumbers(1, 10));
