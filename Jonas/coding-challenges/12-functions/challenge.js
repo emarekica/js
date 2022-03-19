a062c7127265beefcbc4d6c0e2d299fdbd35b147
emarekica <emarekica@gmail.com>
2022-03-19 09:59:02 +0100

    fuctions coding challenge 1 solution

--- Jonas/coding-challenges/12-functions/challenge.js
+++ Jonas/coding-challenges/12-functions/challenge.js
@@ -1,25 +1,69 @@
 "use strict";
 
+// const registerNewAnswer = function () {
+//   let answer = Number(
+//     prompt(`What is your favourite programming language?
+//   0: JavaScript
+//   1: Python
+//   2: Rust
+//   3: C++
+//   (Write option number)`)
+//   );
+//   console.log(answer);
+// };
+
 const poll = {
   question: "What is your favourite programming language?",
   options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
-
-  // This generates [0, 0, 0, 0]. More in the next section!
+  // This generates [0, 0, 0, 0]
   answers: new Array(4).fill(0),
+
+  registerNewAnswer: function () {
+    let answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+      )
+    );
+
+    console.log(answer);
+
+    // update answer
+    // check if it is a number and if the number of options coincide
+    typeof answer === "number" &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults("string");
+  },
+
+  // default is set to array
+  displayResults: function (type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      console.log(`Poll results are ${this.answers.join(", ")}`);
+    }
+  },
 };
 
-// ---- Create a method called 'registerNewAnswer' on the 'poll' object.
+// poll.registerNewAnswer();
 
-// a: Display a prompt window for the user to input the number of the selected option, "What is your favourite programming language?", write option number
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
 
-// b: Based on the input number, update the 'answers' array property. Make sure to check if the input is a number and if the number makes sense
+// for new "this" keyword, use call()
+// we need a new object that contains new array: create an object "newAnswers" that contains it
+poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+// Poll results are 5, 2, 3
+poll.displayResults.call({ answers: [5, 2, 3] });
+// 3) [5, 2, 3]
 
-// ---- Call this method whenever the user clicks the "Answer poll" button
+poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+// Poll results are 5, 2, 3
+poll.displayResults.call({ answers: [5, 2, 3] });
+// (3) [5, 2, 3]
 
-// ---- Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
-
-// ---- Run the 'displayResults' method at the end of each 'registerNewAnswer' method call
-
-// ---- Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll object! So what should the this keyword look like in this situation?
 // test data 1: [5, 2, 3]
 // test data 2: [1, 5, 3, 9, 6, 1]
